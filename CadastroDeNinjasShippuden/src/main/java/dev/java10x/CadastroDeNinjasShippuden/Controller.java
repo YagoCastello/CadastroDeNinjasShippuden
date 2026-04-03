1f361a358b62b02dd58bb5cfb9b4eca186ea08b9
Yago Castello <yagodealmeidacs@gmail.com>
2026-04-03 16:56:23 -0300

    Adicionei o controller

---
+++ CadastroDeNinjasShippuden/src/main/java/dev/java10x/CadastroDeNinjasShippuden/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeNinjasShippuden;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String Bemvindos(){
+        return "Seja bem vindo!!";
+    }
+
+}
