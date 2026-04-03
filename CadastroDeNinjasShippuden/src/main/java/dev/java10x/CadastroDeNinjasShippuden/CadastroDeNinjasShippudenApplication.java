ed4acefb7ef749d6a4b18838b447dedad0dd6656
Yago Castello <yagodealmeidacs@gmail.com>
2026-04-03 15:46:46 -0300

    first commit

---
+++ CadastroDeNinjasShippuden/src/main/java/dev/java10x/CadastroDeNinjasShippuden/CadastroDeNinjasShippudenApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjasShippuden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasShippudenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasShippudenApplication.class, args);
+	}
+
+}
---
+++ CadastroDeNinjasShippuden/src/test/java/dev/java10x/CadastroDeNinjasShippuden/CadastroDeNinjasShippudenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjasShippuden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasShippudenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
