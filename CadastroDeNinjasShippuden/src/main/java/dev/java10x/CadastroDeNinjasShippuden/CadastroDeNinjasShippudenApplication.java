517ae3561ff4d32e07b7d34d9e991fea84966d5f
Yago Castello <yagodealmeidacs@gmail.com>
2026-04-14 20:18:20 -0300

    corrigindo um bug

--- CadastroDeNinjasShippuden/src/main/java/dev/java10x/CadastroDeNinjasShippuden/CadastroDeNinjasShippudenApplication.java
+++ CadastroDeNinjasShippuden/src/main/java/dev/java10x/CadastroDeNinjasShippuden/CadastroDeNinjasShippudenApplication.java
@@ -1,4 +1,4 @@
-package dev.java10x.CadastroDeNinjasShippuden.Ninjas;
+package dev.java10x.CadastroDeNinjasShippuden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
