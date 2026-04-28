2dfc4d1565aa42394cb78a5377b2b0dd4f1a65c2
Yago Castello <yagodealmeidacs@gmail.com>
2026-04-28 16:49:18 -0300

    final, paz

--- CadastroDeNinjasShippuden/src/main/java/dev/java10x/CadastroDeNinjasShippuden/Missoes/MissaoModel.java
+++ CadastroDeNinjasShippuden/src/main/java/dev/java10x/CadastroDeNinjasShippuden/Missoes/MissaoModel.java
@@ -3,11 +3,19 @@ package dev.java10x.CadastroDeNinjasShippuden.Missoes;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjasShippuden.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MissaoModel {
 
     @Id
@@ -25,36 +33,6 @@ public class MissaoModel {
     @JsonIgnore
     private List<NinjaModel> ninjas;
 
-    public MissaoModel(String nome, String dificuldade, List<NinjaModel> ninjas) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninjas = ninjas;
-    }
 
-    public MissaoModel() {
-    }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
